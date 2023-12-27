804c57e353b6e6c57e78582a59e739a3c73bba9e
Baraa Al-Masri <pub@mbaraa.com>
2023-12-27 05:50:09 +0300

    Fixed single commit per day in the generator script

--- templates/text/generate-commits.sh
+++ templates/text/generate-commits.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 FILE_NAME=`uuidgen`.txt
+MAX_COMMIT_COUNT=100
 DATES=( {{ .Dates }} )
 
 touch $FILE_NAME
 for DATE in "${DATES[@]}"; do
-    echo `date` >> $FILE_NAME
-    git add $FILE_NAME
-    git commit -m "🤪 bump commit 😬" --date $DATE
+    for ((i = 0; i < $MAX_COMMIT_COUNT; i++)); do
+        echo `date` >> $FILE_NAME
+        git add $FILE_NAME
+        git commit -m "🤪 bump commit 😬" --date $DATE
+    done
 done
