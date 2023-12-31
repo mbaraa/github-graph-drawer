93a5f01462cab3c1547fdadb63f8cb936f8f2dea
Baraa Al-Masri <pub@mbaraa.com>
2023-12-31 03:01:37 +0300

    Add robots.txt

--- main.go
+++ main.go
@@ -20,7 +20,58 @@ var (
 func main() {
 	templates := template.Must(template.ParseGlob("./templates/html/*"))
 
+	/*
+	   var (
+	   	//go:embed templates/html
+	   	res embed.FS
+
+	   	pages = map[string]string{
+	   		"/": "templates/html/index.html",
+	   	}
+	   )
+
+	   func main() {
+	   	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	   		page, ok := pages[r.URL.Path]
+	   		if !ok {
+	   			w.WriteHeader(http.StatusNotFound)
+	   			return
+	   		}
+	   		tpl, err := template.ParseFS(res, page)
+	   		if err != nil {
+	   			log.Printf("page %s not found in pages cache...", r.RequestURI)
+	   			w.WriteHeader(http.StatusInternalServerError)
+	   			return
+	   		}
+
+	   		w.Header().Set("Content-Type", "text/html")
+	   		w.WriteHeader(http.StatusOK)
+	   		data := map[string]interface{}{
+	   			"userAgent": r.UserAgent(),
+	   		}
+	   		if err := tpl.Execute(w, data); err != nil {
+	   			return
+	   		}
+	   	})
+
+	   	http.FileServer(http.FS(res))
+
+	   	log.Println("server started...")
+	   	err := http.ListenAndServe(":8088", nil)
+	   	if err != nil {
+	   		panic(err)
+	   	}
+	   }
+
+	*/
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/robots.txt" {
+			tmpl, _ := template.ParseFS(res, "resources/robots.txt")
+			w.Header().Set("Content-Type", "text/plain")
+			_ = tmpl.Execute(w, nil)
+			return
+		}
 		w.Header().Set("Content-Type", "text/html")
 		if err := templates.ExecuteTemplate(w, "index", nil); err != nil {
 			log.Errorln(err.Error())
