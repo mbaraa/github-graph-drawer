3da13473b968280781bc84835777318f2da0a56c
Baraa Al-Masri <pub@mbaraa.com>
2024-01-02 01:15:25 +0300

    Add more email schedue stuff

--- utils/emailsched/emailsched.go
+++ utils/emailsched/emailsched.go
@@ -3,6 +3,9 @@ package emailsched
 import (
 	"github-graph-drawer/db"
 	"strings"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func SendScheduleConfirmationEmail(er db.EmailRequest) error {
@@ -15,12 +18,23 @@ func SendScheduleConfirmationEmail(er db.EmailRequest) error {
 	if err != nil {
 		return err
 	}
+	err = db.InsertEmailRequest(er)
+	if err != nil {
+		return err
+	}
 
 	return sendEmail("GitHub Graph Drawer Schedule Confirmation", buf.String(), er.Email)
-
 }
 
-func SendDailyCommitsEmail(es db.EmailScedule) error {
+func ConfirmDailySchedule(er db.EmailRequest) error {
+	err := db.DeleteEmailRequests(er.Token)
+	if err != nil {
+		return err
+	}
+	return StartScheduleForIdkItsLateIHaveNoIdeaWhyImStillAwakeDotDotDot(er)
+}
+
+func SendDailyCommitsEmail(es db.EmailSchedule) error {
 	buf := new(strings.Builder)
 	err := templates.ExecuteTemplate(buf, "daily_commits_reminder", map[string]any{
 		"CommitsCount":        es.CommitsCount,
@@ -34,6 +48,24 @@ func SendDailyCommitsEmail(es db.EmailScedule) error {
 	return sendEmail("GitHub Graph Drawer Daily Reminder", buf.String(), es.Email)
 }
 
-func UnsubscribeFromTheThing(er db.EmailRequest) error {
+func StartScheduleForIdkItsLateIHaveNoIdeaWhyImStillAwakeDotDotDot(er db.EmailRequest) error {
+	for _, date := range er.Dates {
+		err := db.InsertEmailSchedule(db.EmailSchedule{
+			Id:           uuid.NewString(),
+			Email:        er.Email,
+			Token:        er.Token,
+			Date:         date,
+			Message:      er.Message,
+			CommitsCount: er.CommitsCount,
+			CreatedAt:    time.Now().UnixMilli(),
+		})
+		if err != nil {
+			continue
+		}
+	}
 	return nil
+}
+
+func UnsubscribeFromTheThing(email string) error {
+	return db.DeleteEmailRequests(email)
 }
