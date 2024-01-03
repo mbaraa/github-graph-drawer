423461050ce87af9cbcb1f76c5b937acfac0a918
Baraa Al-Masri <pub@mbaraa.com>
2024-01-03 19:37:06 +0300

    Move pages api to a separate file

---
+++ apis/handler.go
@@ -0,0 +1,55 @@
+package apis
+
+import (
+	"net/http"
+	"strings"
+)
+
+type Endpoints map[string]http.HandlerFunc
+
+// IHandler wraps around http.Handler with some extra stuff to be used for each http handler.
+type IHandler interface {
+	// Prefix returns the handler's prefix, that will be ignored by the endpoints map.
+	Prefix() string
+	// Endpoints returns the handler's endpoints and theire {method path} combinations
+	Endpoints() Endpoints
+	// ServeHTTP and off course this one, that implements http.Handler
+	ServeHTTP(http.ResponseWriter, *http.Request)
+}
+
+func NewHandler(
+	endpoints Endpoints,
+	prefix string,
+) IHandler {
+	return &handler{
+		endpoints: endpoints,
+		prefix:    prefix,
+	}
+}
+
+type handler struct {
+	endpoints Endpoints
+	prefix    string
+}
+
+func (h *handler) Prefix() string {
+	return h.prefix + "/"
+}
+
+func (h *handler) Endpoints() Endpoints {
+	return h.endpoints
+}
+
+func (h *handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	endpointPath := strings.TrimPrefix(req.URL.Path, h.Prefix()[:len(h.Prefix())-1])
+	if strings.Contains(endpointPath, "/") {
+		endpointPath = endpointPath[:strings.Index(endpointPath, "/")]
+	}
+	if handler, exists := h.Endpoints()[req.Method+" "+endpointPath]; exists {
+		handler(res, req)
+		return
+	}
+	if req.Method != http.MethodOptions {
+		http.NotFound(res, req)
+	}
+}
---
+++ apis/pages.go
@@ -0,0 +1,52 @@
+package apis
+
+import (
+	"github-graph-drawer/log"
+	"html/template"
+	"io/fs"
+	"net/http"
+	"os"
+)
+
+type pagesApi struct {
+	templates *template.Template
+	resDir    fs.FS
+}
+
+func NewPagesApi() http.Handler {
+	p := &pagesApi{
+		templates: template.Must(template.ParseGlob("./templates/html/*")),
+		resDir:    os.DirFS("./resources"),
+	}
+	endpoints := Endpoints{
+		"GET ":           p.handleHomePage,
+		"GET resources":  p.handleResources,
+		"GET robots.txt": p.handleRobotsFile,
+	}
+	return NewHandler(endpoints, "/")
+}
+
+func (p *pagesApi) handleRobotsFile(w http.ResponseWriter, r *http.Request) {
+	robotsFile, _ := os.ReadFile("./resources/robots.txt")
+	w.Header().Set("Content-Type", "text/plain")
+	_, _ = w.Write(robotsFile)
+}
+
+func (p *pagesApi) handleResources(w http.ResponseWriter, r *http.Request) {
+	filePath := r.URL.Path[1:]
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", http.DetectContentType(file))
+	_, _ = w.Write(file)
+}
+
+func (p *pagesApi) handleHomePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := p.templates.ExecuteTemplate(w, "index", nil); err != nil {
+		log.Errorln(err.Error())
+		return
+	}
+}
